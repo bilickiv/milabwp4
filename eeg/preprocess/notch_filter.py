dce7937841fa6c291c4f0b3e23e27e26a94da490
Fülep Martin Patrik <f.martin.p99@gmail.com>
2024-01-12 16:48:37 +0100

    preprocess - feature extraction teszttel
    EEG modules connected.
    Preprocess, feature extraction, cliques.

--- eeg/preprocess/notch_filter.py
+++
@@ -1,6 +0,0 @@
-from scipy import signal
-
-def get_notch_filtered_data(raw_signal, samp_freq=8192, notch_freq=50, quality_factor=30):
-    b_notch, a_notch = signal.iirnotch(notch_freq, quality_factor, samp_freq)
-    filtered_signal = signal.filtfilt(b_notch, a_notch, raw_signal)
-    return filtered_signal
